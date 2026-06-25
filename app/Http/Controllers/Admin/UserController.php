313bc5a4157a93bc67e83b8da60ca67e7fdb10c7
Meydy Athallah <mmeydyathallah@gmail.com>
2026-06-25 22:40:30 +0700

    Fix missing Auth facade import in UserController

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Services\AuditLogger;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\View\View;
 
